b488c536c1cee7f4acc08c0f7eecf7f1a3bc44ad
richoyouknow <m.richo.saputra69@gmail.com>
2025-12-02 01:22:50 +0700

    login,transaksi,master

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -9,7 +9,7 @@
     <meta content="" name="description">
 
     <!-- Favicon -->
-    <link href="img/favicon.ico" rel="icon">
+    <link href="{{ asset('iStudio-1.0.0/img/favicon.ico') }}" rel="icon">
 
     <!-- Google Web Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -21,14 +21,14 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
 
     <!-- Libraries Stylesheet -->
-    <link href="iStudio-1.0.0/lib/animate/animate.min.css" rel="stylesheet">
-    <link href="iStudio-1.0.0/lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+    <link href="{{ asset('iStudio-1.0.0/lib/animate/animate.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('iStudio-1.0.0/lib/owlcarousel/assets/owl.carousel.min.css') }}" rel="stylesheet">
 
     <!-- Customized Bootstrap Stylesheet -->
-    <link href="iStudio-1.0.0/css/bootstrap.min.css" rel="stylesheet">
+    <link href="{{ asset('iStudio-1.0.0/css/bootstrap.min.css') }}" rel="stylesheet">
 
     <!-- Template Stylesheet -->
-    <link href="iStudio-1.0.0/css/style.css" rel="stylesheet">
+    <link href="{{ asset('iStudio-1.0.0/css/style.css') }}" rel="stylesheet">
 </head>
 
 <body>
